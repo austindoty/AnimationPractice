9eecb5d506cff16ea3ee6004adcb935deda89a59
Austin Doty <asrdoty@gmail.com>
2023-10-25 20:39:39 -0400

    Fix js bugs and html spacing

--- script.js
+++ script.js
@@ -2,40 +2,39 @@ document.addEventListener('DOMContentLoaded', function() {
     const header = document.querySelector('header');
     const container= document.getElementById('container');
     const menuButton = document.getElementById('menu');
-    const links = document.querySelectorAll('a{href^="#"]');
-})
+    const links = document.querySelectorAll('a[href^="#"]');
+
 
 function handleScroll(){
-
-    container.classList.remove ('menuopen');
-    header.classList.toggle ('sticky', window.scrollY >=100);
+    container.classList.remove('menuopen');
+    header.classList.toggle('sticky', window.scrollY >=100);
 }
 
 function handleMenuButtonClick(){
-    header.classList.remnove('sticky');
-    container.classList.toggle ('menuopen')
+    header.classList.remove('sticky');
+    container.classList.toggle('menuopen')
 }
 
 function handleLinkClick(event){
     event.preventDefault();
     const targetId = this.getAttribute('href');
     const targetElement = document.querySelectorAll(targetID);
-    if(targetElement){
+    if (targetElement) {
         targetElement.scrollIntoView({
             behavior: 'smooth'
-        })
+        });
     }
-
+}
     function handleCloseOutside(event){
-        if(!menuButton.contains(event.targer)){
-            container.classList.remove('menuOpen')
+        if(!menuButton.contains(event.target)) {
+            container.classList.remove('menuopen');
             header.classList.add('sticky')
         }
     }
 
-    window.addEventListener('scroll'.handleScroll);
+    window.addEventListener('scroll', handleScroll);
     menuButton.addEventListener('click', handleMenuButtonClick);
     links.forEach( link => link.addEventListener('click', handleLinkClick));
 
     document.addEventListener('click', handleCloseOutside);
-}
+});
